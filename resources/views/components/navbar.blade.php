7ebca3137a6a3fe67a08fb2e131b556543b1b597
velasco-oscar <dev.oscarvelasco@gmail.com>
2025-03-15 14:06:53 -0700

    (Fix) Correciones visuales en la pantalla de inicio

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,7 +3,7 @@
         <div class="flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5">
                 <span class="sr-only">Your Company</span>
-                <img class="h-12 w-auto" src="{{ asset('images/logo.png') }}" alt="">
+                <img class="h-12 w-auto" src="{{ asset('images/logo.png') }}" alt="Mexicali Card Shop">
             </a>
         </div>
         <div class="flex lg:hidden">
@@ -30,7 +30,7 @@
             <div class="flex items-center justify-between">
                 <a href="#" class="-m-1.5 p-1.5">
                     <span class="sr-only">Your Company</span>
-                    <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="">
+                    <img class="h-12 w-auto" src="{{ asset('images/logo.png') }}" alt="Mexicali Card Shop">
                 </a>
                 <button @click="isOpen = false" type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
                     <span class="sr-only">Close menu</span>
@@ -42,13 +42,17 @@
             <div class="mt-6 flow-root">
                 <div class="-my-6 divide-y divide-gray-500/10">
                     <div class="space-y-2 py-6">
-                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50">Product</a>
-                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50">Features</a>
-                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50">Marketplace</a>
-                        <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50">Company</a>
+                    <a href="{{ route('productos.index') }}" class="text-sm font-semibold text-gray-900">Productos</a>
+                        
+                        
+                        
                     </div>
                     <div class="py-6">
-                        <a href="{{ route('login') }}" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold text-gray-900 hover:bg-gray-50">Log in</a>
+                        <a href="{{ route('register') }}" class="text-sm font-semibold text-gray-900">Registrarse</a>
+                    </div>
+
+                    <div class="py-6">
+                        <a href="{{ route('login') }}" class="text-sm font-semibold text-gray-900">Iniciar Sesión <span aria-hidden="true">&rarr;</span></a>
                     </div>
                 </div>
             </div>
